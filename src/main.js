ec56b87023192f2631f7d38aaa988dc8505de378
Chloe Hellberg <chloe.hellberg@gmail.com>
2020-09-08 14:04:45 -0700

    Add a try...catch to handle an API call error
    Co-authored-by: Chloe Hellberg <chloe.hellberg@gmail.com>

--- src/main.js
+++ src/main.js
@@ -15,7 +15,6 @@ $(document).ready(function() {
     const urlZip = `http://api.openweathermap.org/data/2.5/weather?q=${zipCode}&appid=${process.env.API_KEY}`;
 
     request.onreadystatechange = function() {
-      console.log(this.status);
       if (this.readyState === 4 && this.status === 200) {
         const response = JSON.parse(this.responseText);
         getElements(response);
@@ -28,6 +27,32 @@ $(document).ready(function() {
     request.send();
     
     function getElements(response) {
+      
+      let inputtedNumber = response.cod;
+      function codValidate(number) {
+        if (number !== 200) {
+          return new Error("Not a valid number!")
+        } else {
+          return true;
+        }
+      }
+
+      try {
+        const isNumberValid = codValidate(inputtedNumber);
+        if (isNumberValid instanceof Error) {
+          console.error(isNumberValid.message);
+          throw RangeError("Not a valid number!");
+        } else {
+          console.log("Try was successful, so no need to catch!");
+          $('#displayNumber').text("This number is valid. You may continue.")
+        }
+      } catch(error) {
+        console.error(`Red alert! We have an error: ${error.message}`)
+      }
+
+
+
+
       const conversion = new Conversion(response.main.temp, response.visibility);
       conversion.calculateTemp();
       conversion.calculateVisibility();
@@ -36,7 +61,7 @@ $(document).ready(function() {
       $('.showFahrenheit').text(`The temperature in Fahrenheit is ${conversion.calculateTempToFahrenheit} degrees.`);
       $('.showWindSpeed').text(`The wind speed is ${response.wind.speed} mph`);
       $('.showWindGust').text(`The wind gust is ${response.wind.gust} mph`);
-      $('.showVisibility').text(`The current visibility is ${response.visibility} meters and ${conversion.calculateToMiles} miles.`);
+      $('.showVisibility').text(`The current visibility is ${response.visibility} meters or ${conversion.calculateToMiles} miles.`);
       $('.showClouds').text(`The current cloud cover is ${response.clouds.all}%`);
     }
   });
