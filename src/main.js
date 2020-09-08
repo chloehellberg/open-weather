28a83f5c48f852f4bd18f62e7b311ad59bc5cdd9
Chloe Hellberg <chloe.hellberg@gmail.com>
2020-09-08 11:56:21 -0700

    Conversion class added and method to convert meters to miles
    Co-authored-by: Jeff Dinsmore <hello@jeffdinsmore.com>

--- src/main.js
+++ src/main.js
@@ -13,6 +13,7 @@ $(document).ready(function() {
     const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
 
     request.onreadystatechange = function() {
+      console.log(this.status);
       if (this.readyState === 4 && this.status === 200) {
         const response = JSON.parse(this.responseText);
         getElements(response);
@@ -24,14 +25,15 @@ $(document).ready(function() {
     request.send();
     
     function getElements(response) {
-      const conversion = new Conversion(response.main.temp);
+      const conversion = new Conversion(response.main.temp, response.visibility);
       conversion.calculateTemp();
+      conversion.calculateVisibility();
       $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
       $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
       $('.showFahrenheit').text(`The temperature in Fahrenheit is ${conversion.calculateTempToFahrenheit} degrees.`);
       $('.showWindSpeed').text(`The wind speed is ${response.wind.speed} mph`);
       $('.showWindGust').text(`The wind gust is ${response.wind.gust} mph`);
-      $('.showVisibility').text(`The current visibility is ${response.visibility} meters.`);
+      $('.showVisibility').text(`The current visibility is ${response.visibility} meters and ${conversion.calculateToMiles} miles.`);
       $('.showClouds').text(`The current cloud cover is ${response.clouds.all}%`);
     }
   });
--- src/scripts.js
+++ src/scripts.js
@@ -1,11 +1,17 @@
 export default class Conversion {
-  constructor(temperature) {
+  constructor(temperature, visibility) {
     this.temperature = temperature;
+    this.visibility = visibility;
   }
 
   calculateTemp() {
     this.calculateTempToFahrenheit = (((this.temperature - 273.15) * 1.8) + 32).toFixed(1);
     return this.calculateTempToFahrenheit;
+  }
+
+  calculateVisibility() {
+    this.calculateToMiles = (this.visibility / 1609.344).toFixed(2);
+    return this.calculateToMiles;
   }
 
 }
